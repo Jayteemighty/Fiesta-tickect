899e8815d6ba42412968b690cb861cc74945a140
Adesina Joshua <tolujosh1@gmail.com>
2024-08-29 12:46:38 +0200

    adding prod and local dabase

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -1,7 +1,26 @@
 from django.contrib import admin
 from django.urls import path, include
 
+from django.conf import settings
+from django.conf.urls.static import static
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title='Fiesta API Docs',
+        default_version='v1',
+    )
+)
+
 urlpatterns = [
-    path("admin/", admin.site.urls),
+    path("1234567890/admin/", admin.site.urls),
+    path('docs/', schema_view.with_ui('swagger', cache_timeout=0), name='api_docs'), #documentation
+    
+    # Apps Api Url
     path("api/v1/user/", include('user.urls')),
 ]
+
+urlpatterns += staticfiles_urlpatterns()
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) 

a40f9e804fc07249cc576a0d2340465bfeeb6992
Adesina Joshua <tolujosh1@gmail.com>
2024-09-03 10:09:18 +0100

    adjust serializers

--- backend/events/serializers.py
+++ backend/events/serializers.py
@@ -2,6 +2,11 @@ from rest_framework import serializers
 from .models import Event
 
 class EventSerializer(serializers.ModelSerializer):
+    created_by = serializers.ReadOnlyField(source='created_by.username')
+
     class Meta:
         model = Event
-        fields = '__all__'
+        fields = [
+            'id', 'name', 'description', 'location', 'start_date', 'end_date',
+            'start_time', 'end_time', 'price', 'payment_method', 'created_by',
+        ]
